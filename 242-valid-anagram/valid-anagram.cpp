294ad77c6b96f979dc1050ee68f796c562af3855
Abhinav Sandeep Todmal <138814979+Abhinavtodmal@users.noreply.github.com>
2025-07-26 09:57:34 +0530

    Time: 4 ms (39.17%) | Memory: 9.7 MB (48.55%) - LeetSync

---
+++ 242-valid-anagram/valid-anagram.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+         
+         unordered_map<char ,int> mp ;
+        
+        if(s.length() != t.length()) return false;
+
+         for(auto it : s )
+         {
+            mp[it]++;
+         }
+
+         for(auto c : t)
+         {
+             mp[c]--;
+         }
+
+         
+          for(auto it : mp) {
+            if(it.second != 0) return false;
+        }
+         return true;
+    }
+};
