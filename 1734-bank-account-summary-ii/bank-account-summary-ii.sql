a140faaeeeb6998890b5f939187f64a71fa36b95
Abhinav Sandeep Todmal <138814979+Abhinavtodmal@users.noreply.github.com>
2025-08-24 10:12:45 +0530

    Time: 768 ms (72.03%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1734-bank-account-summary-ii/bank-account-summary-ii.sql
@@ -0,0 +1,8 @@
+# Write your MySQL query statement below
+
+select Users.name , sum(amount) as balance 
+from Users 
+inner join Transactions on 
+       Users.account = Transactions.account 
+group by Users.account 
+having balance > 10000;
